cb0065679830f26c7195eb3533f803c7c62a7d5f
Jingwennnn <wongjingwen1234@gmail.com>
2025-09-21 22:23:39 +0800

    updated voice assistant

--- foodwise-dashboard/src/components/shared/KommunicateChat.tsx
+++ foodwise-dashboard/src/components/shared/KommunicateChat.tsx
@@ -22,7 +22,7 @@ const KommunicateChat: React.FC = () => {
                 }
 
                 const kommunicateSettings = {
-                    appId: "1ec35be68b280072a0d03a5e94e36ad89",
+                    appId: "28a0c256e4ce3943f8f584845e5cc1980",
                     popupWidget: true,
                     automaticChatOpenOnNavigation: true
                 };
